1e4293a50c8082cf03563248dd09a4cb468cf80f
laduy.hcmus <laduy.hcmus@gmail.com>
2013-03-19 04:16:47 +0000

    DP-CaseStudy 1 Câu a: BieuThucDiagram.cd và các lớp: BieuThuc.cs, HangSo.cs, DonThuc.cs, DaThuc.cs, DaThucTong.cs, DaThucTich.cs, DaThucThuong.cs Câu b: Hàm constructor cho các lớp (Còn câu c và d)

---
+++ Source/DaoHamV1.3/DaoHamV1.3/DaThucTich.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DaoHamV1._3
+{
+    public class DaThucTich : DaThuc
+    {
+        // CONSTRUCTORS
+        public DaThucTich(BieuThuc _u, BieuThuc _v)
+        {
+            u = _u;
+            v = _v;
+        }
+
+        // TÍNH GIÁ TRỊ + ĐẠO HÀM
+        public override double TinhGiaTri()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override BieuThuc TinhDaoHam()
+        {
+            throw new NotImplementedException();
+        }
+
+        // BIỂU DIỄN BIỂU THỨC
+        public override string ToString()
+        {
+            return ("(" + u.ToString() + " * " + v.ToString() + ")");
+        }
+    }
+}
---
+++ Source/DaoHamV1.3/DaoHamV1.3/DonThuc.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DaoHamV1._3
+{
+    public class DonThuc : BieuThuc
+    {
+        private double x;
+        private double heSo;
+        private int soMu;
+
+        // CONSTRUCTORS
+        public DonThuc()
+        {
+            x = 1;
+            heSo = 1;
+            soMu = 1;
+        }
+
+        public DonThuc(double _x, double _heSo, int _soMu)
+        {
+            x = _x;
+            heSo = _heSo;
+            soMu = _soMu;
+        }
+
+        // TÍNH ĐẠO HÀM + GIÁ TRỊ
+        public override double TinhGiaTri()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override BieuThuc TinhDaoHam()
+        {
+            throw new NotImplementedException();
+        }
+
+        // BIỂU DIỄN BIỂU THỨC
+        public override string ToString()
+        {
+            return (heSo + "X ^ " + soMu);
+        }
+    }
+}

dd64899b557cd441037100142f719d9984325e7b
nkluan.gl <nkluan.gl@gmail.com>
2013-06-09 14:16:19 +0000

    Tạo các lớp hình học. kế thừa từ lớp MyShape.cs.

---
+++ BaiTapTuan/Drawfigure/Tuan8/CodeSubmit/BT-DoHoa-Nhom20/Shape/LineEx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BT_DoHoa_Nhom20.Shape
+{
+    class LineEx:MyShape
+    {
+        public double x1;
+        public double x2;
+        public double y1;
+        public double y2;
+
+        public LineEx(double X1, double Y1, double X2, double Y2) {
+            this.x1 = X1;
+            this.x2 = X2;
+            this.y1 = Y1;
+            this.y2 = Y2;
+        }
+        public override void Draw()
+        {
+            myGraphic.DrawLine(x1, y1, x2, y2);
+        }
+    }
+}
---
+++ BaiTapTuan/Drawfigure/Tuan8/CodeSubmit/BT-DoHoa-Nhom20/Shape/MyShape.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BT_DoHoa_Nhom20.Shape
+{
+    public class MyShape
+    {
+        protected GraphicLibExt myGraphic;
+
+        public GraphicLibExt MyGraphic{
+            set { myGraphic = value; }
+        }
+        public virtual void Draw() { }
+    }
+}

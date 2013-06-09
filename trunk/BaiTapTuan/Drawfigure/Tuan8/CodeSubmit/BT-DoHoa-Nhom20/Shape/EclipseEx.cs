823ea15781a9756337e96c88dc9600d3e0541643
nkluan.gl@gmail.com <nkluan.gl@gmail.com@d1c5c3df-abd6-13b5-fc16-f8bc8581669e>
2013-06-09 14:16:19 +0000

    Tạo các lớp hình học. kế thừa từ lớp MyShape.cs.

---
+++ trunk/BaiTapTuan/Drawfigure/Tuan8/CodeSubmit/BT-DoHoa-Nhom20/Shape/EclipseEx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BT_DoHoa_Nhom20.Shape
+{
+    class EclipseEx:MyShape
+    {
+        private double x;
+        private double y;
+        private double width;
+        private double height;
+        public EclipseEx(double X, double Y, double WIDTH, double HEIGHT) {
+            this.x = X;
+            this.y = Y;
+            this.width = WIDTH;
+            this.height = HEIGHT;
+        }
+        public override void Draw()
+        {
+            myGraphic.DrawEclipse(x, y, width, height);
+        }
+    }
+}
---
+++ trunk/BaiTapTuan/Drawfigure/Tuan8/CodeSubmit/BT-DoHoa-Nhom20/Shape/RectangleEx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BT_DoHoa_Nhom20.Shape
+{
+    class RectangleEx:MyShape
+    {
+    private double x;
+        private double y;
+        private double width;
+        private double height;
+        public RectangleEx(double X, double Y, double WIDTH, double HEIGHT)
+        {
+            this.x = X;
+            this.y = Y;
+            this.width = WIDTH;
+            this.height = HEIGHT;
+        }
+        public override void Draw()
+        {
+            myGraphic.DrawRectangle(x, y, width, height);
+        }
+    }
+}

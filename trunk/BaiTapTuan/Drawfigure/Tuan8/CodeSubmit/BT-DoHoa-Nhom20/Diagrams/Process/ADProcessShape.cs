c32ab528efec9d690f02d7d6ee6511652bdb4c95
thlinh2308@gmail.com <thlinh2308@gmail.com@d1c5c3df-abd6-13b5-fc16-f8bc8581669e>
2013-06-10 16:31:01 +0000

    [3] Hoàn tất sơ đồ class

--- trunk/BaiTapTuan/Drawfigure/Tuan8/CodeSubmit/BT-DoHoa-Nhom20/Diagrams/Process/ADProcessShape.cs
+++ trunk/BaiTapTuan/Drawfigure/Tuan8/CodeSubmit/BT-DoHoa-Nhom20/Diagrams/Process/ADProcessShape.cs
@@ -7,5 +7,9 @@ namespace BT_DoHoa_Nhom20
 {
     public class ADProcessShape : ProcessShape
     {
+        public override void Draw()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
